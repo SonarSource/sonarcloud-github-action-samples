ccd8f1d251b56a5b8f7ffbb714427e638b7a1213
Saurabh Oza <41860110+onlyfullstack@users.noreply.github.com>
2021-02-02 17:25:18 +0000

    Update HelloWorldTests.java

--- src/test/java/HelloWorldTests.java
+++ src/test/java/HelloWorldTests.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class HelloWorldTests {
+class HelloWorldTests { 
   @Test
   void sayHello() {
     assertEquals("hello world1", new HelloWorld().say());
